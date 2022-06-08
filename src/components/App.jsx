fe742b1f08f0d80d1caa90c068968aba6371559f
Soloman Garcia <solomangarcia@Solomans-MacBook-Air.local>
2022-06-07 18:30:17 -0600

    debug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+//import { Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <CssBaseline />
+      <h1>Hello, World - filmpire</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
